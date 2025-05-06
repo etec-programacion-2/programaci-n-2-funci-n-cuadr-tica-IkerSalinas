854bdfaa382d3dd3e9dbe79eb27a23ab02ec9e63
Noah <noahlucianograna@gmail.com>
2025-05-06 08:35:51 -0300

    feat: Cálculo de valores de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -14,4 +14,15 @@ fun main() {
     }
 
     println("La función es: $a x² + $b x + $c")
+    println("x \t\t f(x)")
+    for (x in -5..5) {
+        val resultado = calcularFuncion(x, a, b, c)
+
+        println("$x \t\t $resultado")
+    }
 }
+
+fun calcularFuncion(x: Int, a: Int, b: Int, c: Int): Int {
+    return a * x * x + b * x + c
+}
+
