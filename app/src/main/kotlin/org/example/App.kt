84a2df681dad935c4f50782f7934a80acc153a1c
Noah <noahlucianograna@gmail.com>
2025-05-06 08:25:23 -0300

    feat: Estructura básica y definición de coeficientes

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,17 @@
+
+package org.example
+
+fun main() {
+
+    val a = 2
+    val b = 4
+    val c = 6
+
+
+    if (a == 0) {
+        println("a no puede ser cero.")
+        return
+    }
+
+    println("La función es: $a x² + $b x + $c")
+}
